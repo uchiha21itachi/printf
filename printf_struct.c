3f9ce1e69ead5cc393169a93b8a1b5e4765a16ec
Yash SHARMA <yassharm@e1r5p16.42.fr>
2020-02-03 22:19:21 +0100

    Changed nothing important in printf_struct.c

--- printf_struct.c
+++ printf_struct.c
@@ -1,7 +1,51 @@
-typedef struct	flag_list
-{
-	char			c;
+#include "ft_printf.h"
+#include <stdlib.h>
 
-	void			*content;
-	struct s_list	*next;
-}				flag_list;
+typedef struct	f_list
+{
+	char				c;
+	struct f_list	*next;
+}					f_list;
+
+
+f_list	*ft_lstnew(char ch)
+{
+	f_list	*new;
+
+	new = (f_list *)malloc(sizeof(f_list));
+	if (new == 0)
+		return (NULL);
+	new->c = ch;
+	new->next = NULL;
+	return (new);
+}
+
+void	ft_lstadd_back(f_list **alst, char c)
+{
+	f_list *ls;
+
+	if (!alst)
+		return ;
+	if (*alst)
+		ls = *alst;
+	else
+	{
+		*alst = ft_lstnew(c);
+		return ;
+	}
+	while (ls->next != NULL)
+		ls = ls->next;
+	ls->c = c;
+	ls->next = NULL;
+}
+
+int main()
+{
+	char c;
+	f_list	*alst;
+	f_list	*new;
+
+	
+	new = ft_lstadd_back(&alst, 'i');
+	
+}
