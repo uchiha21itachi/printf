622cb889d16ab2dc1f52fdf3ad61cf230304eb6c
Yash SHARMA <yassharm@e2r8p15.42.fr>
2020-02-04 22:56:47 +0100

    Did some testing on 's' 'd' and 'i' for precision and width

--- ft_printf_tests.c
+++ ft_printf_tests.c
@@ -67,12 +67,71 @@ void	test3(void)
 
 }
 
+void	s_conversion(void)
+{
+	//precision > width 
+	//1.1 precision > length && width > length  
+	printf ("%20.30s\n", "Hello world!!");
+	//1.2 precision > length && width < length  
+	printf ("%5.30s\n", "Hello world!!");
+	//1.3 precision < length && width < length 
+	printf ("%9.10s\n", "Hello world!!");
+	
+	//width > precision
+	//2.1 precision > length && width > length
+	printf ("%20.15s\n", "Hello world!!");
+	//2.2 precision < length && width > length
+	printf ("%20.10s\n", "Hello world!!");
+	//2.3 precision < length && width < length 
+	printf ("%10.5s\n", "Hello world!!");
+	
+	//width == precision
+	//3.1. width > length && precision > length 
+	printf ("%30.30s\n", "Hello world!!");
+	//3.2 width < length && precision < length 
+	printf ("%10.10s\n", "Hello world!!");
+}
+
+void	i_and_d_conversion(void)
+{
+	//precision > width
+	//1.1 precision > num_length && width > num_length	
+	printf ("%10.15d\n", 12203);
+	//1.2 precision < num_length && width < num_length	
+	printf ("%2.4d\n", 12203);
+	//1.3 precision > num_length && width < num_length	
+	printf ("%2.10d\n", 12203);
+
+	//width > precision
+	//2.1 precision > num_length && width > num_length	
+	printf ("%15.10d\n", 12203);
+	//2.2 precision < num_length && width < num_length	
+	printf ("%4.2d\n", 12203);	
+	//2.3 precision < num_length && width > num_length	
+	printf ("%10.2d\n", 12203);	
+
+	//width == precision
+	//3.1 precision > num_length && width > num_length	
+	printf ("%10.10d\n", 12203);
+	//3.2 precision < num_length && width < num_length	
+	printf ("%4.4d\n", 12203);
+
+}
+
 
 int main()
 {
-	// test1();
-	printf ("%-10s\n", "Hello world!!");
-	// test2();
-	// while (1);
-	// test3();
+	int a = 2012;
+	void *k = &a;
+
+	printf ("%x\n",k);
+	printf ("%d\n",a);
 }
+
+
+
+
+
+
+
+
