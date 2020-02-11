2825f5d24c1d23b2b7b1b7a427831e15fc9876b2
Yash SHARMA <yassharm@e1r12p18.42.fr>
2020-02-11 19:39:08 +0100

    Strings conversion not working

--- ft_printf_strings.c
+++ ft_printf_strings.c
@@ -10,4 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 
+
+f_list	convert_strings(f_list block, const char *format, int i, va_list arg_list)
+{
+	char *s;
+	int k;
+
+	printf ("\n\n\n");
+	k = 0;
+	while ((s = va_arg(arg_list, char *)))
+	{
+	    printf ("%c\n", s[k]);
+		k++;
+	}
+	return (block);
+}
