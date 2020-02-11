91ea117150d17393f3d8ce29c43f916607cc7878
Yash SHARMA <yash221b@gmail.com>
2020-02-11 23:42:35 +0100

    Parsing and output working for c conversion. Conversion of D and I still on progress

--- ft_printf_numbers.c
+++ ft_printf_numbers.c
@@ -12,12 +12,83 @@
 
 #include "ft_printf.h"
 
+int		ft_num_len(num)
+{
+	int length;
+
+	length = 0;
+	if (num >= 0)
+	{
+		while (num >= 10)
+		{
+			num = num/10;
+			length++;
+		}
+		length++;
+	}
+	return(length);
+}
+
+void		create_output(int num, int num_len, f_list block)
+{
+	// int i;
+	// if (block.p_avail == 1)
+	// {
+	// 	if (block.precision > block.width)
+	// 	{
+	// 		if ((block.precision > num_len && block.width > num_len) || (block.precision > num_len && block.width < num_len))
+	// 		{
+	// 			i = -1;
+	// 			while (++i < (block.precision - num_len))
+	// 				block.count += write(1, "0", 1);
+	// 			printf ("%d", num);+
+	// 		}
+	// 		else if (block.precision <= num_len && block.width <= num_len)
+	// 		{
+	// 			printf ("")
+	// 		}
+	// 	}
+	// 	else if (block.width > block.precision)
+	// 	{
+	// 		if (block.precision > )
+	// 	}
+		
+	// }
+	// else
+	// {
+		
+	// }
+	
+}
+
 f_list	convert_digit(f_list block, const char *format, int i, va_list arg_list)
 {
 	int num;
+	int num_len;
 
 	num = va_arg(arg_list, int);
-	printf ("%d\n", num);
-
+	num_len = ft_num_len(num);
+	create_output(num, num_len, block);
 	return (block);
 }
+
+
+/*
+if only width 
+	if width < num_len
+	if width > num_len
+if only precision 
+	if p < num_len 
+	if p > num_len
+*/
+
+
+
+
+
+// //if precision is available then
+// 	if p <= num_len
+// 		print just number
+// 	if p > num_len 
+// 		print zeros before number 
+	
---
+++ parse_c.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_c.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yassharm <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/11 22:11:57 by yassharm          #+#    #+#             */
+/*   Updated: 2020/02/11 22:12:00 by yassharm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+f_list	parse_c(f_list block, const char *format, int i, va_list arg_list)
+{
+	int space_count;
+	int c;
+
+	c = va_arg(arg_list, int);
+	space_count = 0;
+	if (block.minus_flag)
+	{
+		block.count += write(1, &c, 1);
+		while (space_count++ < (block.width - 1))
+			block.count += write(1, " ", 1);
+	}
+	else
+	{
+		while (space_count++ < (block.width - 1))
+			block.count += write(1, " ", 1);
+		block.count += write(1, &c, 1);
+	}
+	return (block);
+}
